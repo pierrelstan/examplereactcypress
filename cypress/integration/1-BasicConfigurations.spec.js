bf2e3243e39672387ac6aceaa7713366456075b3
Stanley Pierre-Louis <stanleypl75@gmail.com>
2022-01-16 22:04:26 -0400

    test: basic configurations

---
+++ cypress/integration/1-BasicConfigurations.spec.js
@@ -0,0 +1,13 @@
+import describeOnBranches from '../utils/describeOnBranches';
+
+describeOnBranches('basic-configurations')('Basic Configurations', () => {
+  it('Commintlint.config.js exist', () => {
+      cy.readFile('commitlint.config.js')
+  });
+  it('Cypress.json exist', () => {
+    cy.readFile('cypress.json')
+});
+it('.eslintrc.json exist', () => {
+  cy.readFile('.eslintrc.json')
+});
+});
